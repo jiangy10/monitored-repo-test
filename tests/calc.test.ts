a99a8be1efb583ce07f959aed24d2ea299e7a478
Yuxuan Jiang <jiangy10@rose-hulman.edu>
2024-06-05 16:29:06 +0800

    wps client test

--- tests/calc.test.ts
+++
@@ -1,7 +0,0 @@
-import { add } from '../functions/calc';
-
-describe('add function tests', () => {
-    test('adds 1 + 2 to equal 3 (passing test)', () => {
-      expect(add(1, 2)).toBe(3);
-    });
-  });
